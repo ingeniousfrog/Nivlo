2b9f224640f7f9dc713aff16520cb62c4edfb2b1
heqikai <heqikai@myhexin.com>
2026-06-22 13:21:26 +0800

    fix: link AVKit for video playback

--- Package.swift
+++ Package.swift
@@ -36,6 +36,9 @@ let package = Package(
         "NivloImaging",
         "NivloIndexing",
         "NivloPersistence",
+      ],
+      linkerSettings: [
+        .linkedFramework("AVKit")
       ]
     ),
     .testTarget(
